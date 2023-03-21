7997f6a2c0e8c00f3af00413a287e53be6559d8c
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2023-03-21 22:53:38 +0300

    Update Gate.java

--- lab4-assignment-gate-a-skeleton/app/src/main/java/com/company/gate/logic/Gate.java
+++ lab4-assignment-gate-a-skeleton/app/src/main/java/com/company/gate/logic/Gate.java
@@ -5,5 +5,58 @@ package com.company.gate.logic;
  */
 public class Gate {
     // TODO -- Fill in your code here
+    public static final int IN = 1;
+    public static final int OUT = -1;
+    public static final int CLOSED = 0;
 
+    private int mSwing;
+
+    public Gate() {
+        mSwing = CLOSED;
+    }
+
+    public int getSwingDirection() {
+        return mSwing;
+    }
+
+    public boolean setSwing(int direction) {
+        if(direction == IN || direction == OUT ||  direction == CLOSED) {
+            mSwing = direction;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean open(int direction) {
+        if (direction == IN || direction == OUT) {
+            this.setSwing(direction);
+            return true;
+        }
+        return false;
+    }
+
+    public void close() {
+        mSwing = CLOSED;
+    }
+
+    public int thru(int count) {
+        if(mSwing == IN)    return count;
+        if(mSwing == OUT)   return -count;
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        if (mSwing == CLOSED) {
+            return "This gate is closed";
+        }
+        if (mSwing == IN) {
+            return "This gate is open and swings to enter the pen only";
+        }
+        if (mSwing == OUT) {
+            return "This gate is open and swings to exit the pen only";
+        }
+        return "This gate has an invalid swing direction";
+
+    }
 }
